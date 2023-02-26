dcda627d2fe51d83ba89b17f9fb308ac315a9f13
Baoshuo Ren <i@baoshuo.ren>
2023-02-26 14:40:33 +0800

    sjzezoj.com__real_zan_cnt: add logs

--- sjzezoj.com__real_zan_cnt.user.js
+++ sjzezoj.com__real_zan_cnt.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         S2OJ 显示真实点赞数
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.1.1
 // @description  启用该脚本后可以显示 S2OJ 中真实的点赞数量。
 // @author       Baoshuo
 // @match        https://sjzezoj.com/*
@@ -12,12 +12,16 @@
 
 (function() {
     'use strict';
+    
+    console.log('Loaded "S2OJ 显示真实点赞数" userscript.\n\nhttps://github.com/renbaoshuo/userscripts\n');
 
     var original_click_zan_block_fn = $.fn.click_zan_block;
 
     $.fn.click_zan_block = function() {
         return original_click_zan_block_fn.call(this).each(function() {
             var zan_cnt = $('.uoj-click-zan-cnt', this);
+            
+            console.log('Patched:', zan_cnt.text(), zan_cnt.attr('title'));
 
             zan_cnt.html('[<strong>' + zan_cnt.attr('title') + '</strong>]');
         });
@@ -28,6 +32,8 @@
             var zan = $(this).attr('title');
 
             if (!zan) return;
+
+            console.log('Patched:', $(this).text(), zan);
 
             $(this).html('[<strong>' + zan + '</strong>]');
         });
